876e4ec4d30459aa38728713a721c66fd0d090b2
Eric Chen <echen1@salesforce.com>
2020-04-09 18:47:13 -0700

    use server for web dyno

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Company
+from models import *
 
 @app.route("/")
 def hello():
