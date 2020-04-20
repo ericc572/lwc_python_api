830a0d8b1c4afde2a114be7b9cfd93a23f64c89d
oliverwang101 <oliverlw1336@gmail.com>
2020-04-20 14:14:00 -0700

    data validation and file open error handle

--- job_pipeline.py
+++ job_pipeline.py
@@ -3,8 +3,9 @@ import os
 
 from app import db
 from models import *
-
-with open('data.json') as json_file:
+jobFile = 'data.json'
+if (os.path.getsize(jobFile) > 0):
+    json_file = open(jobFile,'r')
     data = json.load(json_file)
     for p in data:
         title = p['title']
@@ -21,3 +22,4 @@ with open('data.json') as json_file:
         db.session.add(job_listing)
         db.session.commit()
         print("job listing added. Job id={}".format(job_listing.id))
+
